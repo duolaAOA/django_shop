7291187c0e1960c14009535c43669343e04989ed
duolaAOA <1219960386@qq.com>
2018-03-30 18:04:55 +0800

    add user mobilebackend

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+# -*-coding:utf-8 -*-
 
-# Create your views here.
+from django.contrib.auth import get_user_model
+from django.contrib.auth.backends import ModelBackend
+from django.db.models import Q
+
+
+class CustomBackend(ModelBackend):
+    """
+    重写
+    自定义认证
+    # http://python.usyiyi.cn/translate/django_182/topics/auth/customizing.html
+    """
+    def authenticate(self, request, username=None, password=None, **kwargs):
+        Usermodel = get_user_model()
+        try:
+            # user = Usermodel.objects.get(email=email)
+            user = Usermodel.objects.get(Q(username=username) | Q(mobile=username))   # 并集查询
+        except Usermodel.DoesNotExist:
+            return None
+        else:
+            if user.check_password(password):
+                return user
+        return None
