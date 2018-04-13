da3dd27d4cbc4c9c657de2c4537d87f3aaf1a077
duolaAOA <1219960386@qq.com>
2018-04-13 20:11:11 +0800

    Add operation permission validation.

--- apps/user_operation/views.py
+++ apps/user_operation/views.py
@@ -5,12 +5,15 @@ from rest_framework.permissions import IsAuthenticated
 
 from .models import UserFav
 from .serializers import UserFavSerializer
+from utils.permissions import IsOwnerOrReadOnly
 
 
 class UserFavViewset(mixins.CreateModelMixin, mixins.ListModelMixin, mixins.DestroyModelMixin, viewsets.GenericViewSet):
     """
     用户收藏功能
-    """
-    queryset = UserFav.objects.all()
-    permission_classes = (IsAuthenticated, )
+    """ 
+    permission_classes = (IsAuthenticated, IsOwnerOrReadOnly)
     serializer_class = UserFavSerializer
+
+    def get_queryset(self):
+        return UserFav.objects.filter(user=self.request.user)
