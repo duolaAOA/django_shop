ba5749d678d7c72e39cd5f6b918dd2bc36740b3d
duolaAOA <1219960386@qq.com>
2018-04-13 20:38:07 +0800

    add SessionAuthentication

--- apps/user_operation/views.py
+++ apps/user_operation/views.py
@@ -3,6 +3,7 @@ from rest_framework import viewsets
 from rest_framework import mixins
 from rest_framework.permissions import IsAuthenticated
 from rest_framework_jwt.authentication import JSONWebTokenAuthentication
+from rest_framework.authentication import SessionAuthentication
 
 from .models import UserFav
 from .serializers import UserFavSerializer
@@ -15,7 +16,7 @@ class UserFavViewset(mixins.CreateModelMixin, mixins.ListModelMixin, mixins.Dest
     """
     permission_classes = (IsAuthenticated, IsOwnerOrReadOnly)
     serializer_class = UserFavSerializer
-    authentication_classes = (JSONWebTokenAuthentication, )
+    authentication_classes = (JSONWebTokenAuthentication, SessionAuthentication)
 
     def get_queryset(self):
         return UserFav.objects.filter(user=self.request.user)
