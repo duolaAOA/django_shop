c34a3dc2350f21a7d27ac5c5cf6cd0584f31a8b7
duolaAOA <1219960386@qq.com>
2018-04-13 19:19:50 +0800

    添加用户是否认真登录权限验证

--- apps/user_operation/views.py
+++ apps/user_operation/views.py
@@ -1,14 +1,16 @@
 # -*- coding: utf-8 -*-
 from rest_framework import viewsets
 from rest_framework import mixins
+from rest_framework.permissions import IsAuthenticated
 
 from .models import UserFav
 from .serializers import UserFavSerializer
 
 
-class UserFavViewset(mixins.CreateModelMixin, mixins.DestroyModelMixin, viewsets.GenericViewSet):
+class UserFavViewset(mixins.CreateModelMixin, mixins.ListModelMixin, mixins.DestroyModelMixin, viewsets.GenericViewSet):
     """
     用户收藏功能
     """
     queryset = UserFav.objects.all()
+    permission_classes = (IsAuthenticated, )
     serializer_class = UserFavSerializer
