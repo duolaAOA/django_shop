dced7f07cd4f338f2a5b6fb40b4be803a9714bb3
duolaAOA <1219960386@qq.com>
2018-04-15 14:14:54 +0800

    add user fav serializer

--- apps/user_operation/serializers.py
+++ apps/user_operation/serializers.py
@@ -3,6 +3,18 @@ from rest_framework import serializers
 from rest_framework.validators import UniqueTogetherValidator
 
 from .models import UserFav
+from goods.serializer import GoodsSerializer
+
+
+class UserFavDetailSerializer(serializers.ModelSerializer):
+    """
+    用户收藏详情
+    """
+    goods = GoodsSerializer()
+
+    class Meta:
+        model = UserFav
+        fields = ("goods", "id", )
 
 
 class UserFavSerializer(serializers.ModelSerializer):
--- apps/user_operation/views.py
+++ apps/user_operation/views.py
@@ -6,18 +6,30 @@ from rest_framework_jwt.authentication import JSONWebTokenAuthentication
 from rest_framework.authentication import SessionAuthentication
 
 from .models import UserFav
-from .serializers import UserFavSerializer
+from .serializers import UserFavSerializer, UserFavDetailSerializer
 from utils.permissions import IsOwnerOrReadOnly
 
 
 class UserFavViewset(mixins.CreateModelMixin, mixins.ListModelMixin, mixins.DestroyModelMixin, viewsets.GenericViewSet):
     """
-    用户收藏功能
+    list:
+        获取用户收藏列表
+    retrieve:
+        判断商品是否已收藏
+    create:
+        收藏商品
     """
     permission_classes = (IsAuthenticated, IsOwnerOrReadOnly)
-    serializer_class = UserFavSerializer
-    authentication_classes = (JSONWebTokenAuthentication, SessionAuthentication)
+    authentication_classes = (JSONWebTokenAuthentication, SessionAuthentication,)
     lookup_field = "goods_id"
   
     def get_queryset(self):
         return UserFav.objects.filter(user=self.request.user)
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return UserFavDetailSerializer
+        elif self.action == "create":
+            return UserFavSerializer
+
+        return UserFavSerializer
